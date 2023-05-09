480f624c6a84526a6e61c428c93c7fcb77b8c469
Diego Novoa <diegoalbanovoa@gmail.com>
2023-05-09 06:13:40 -0500

    Reportes de Ejecucion

---
+++ Test/Pruebas Unitarias/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+
+
+@pytest.fixture(scope='session')
+def html_report_title():
+    return 'Informe de pruebas de la aplicacion Ecommerce'
+
+@pytest.mark.optionalhook
+def pytest_html_results_table_header(cells):
+    cells.insert(2, html_report_title)
+
+@pytest.mark.optionalhook
+def pytest_html_results_table_row(report, cells):
+    cells.insert(2, report.description)
+
+def pytest_configure(config):
+    config._metadata['Proyecto'] = 'Ecommerce'
+    config._metadata['Tester'] = 'Diego-Novoa-Jairo-Perez'
+
+def pytest_html_report_title(report):
+    report.title = "Informe de pruebas de la aplicacion Ecommerce"
+
+def pytest_html_results_table_row(report, cells):
+    print(report)
+
